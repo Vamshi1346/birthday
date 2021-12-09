3181143e1644437fb420615c9194eb5c2f4a58cc
unknown <vamshireddy1346@gmail.com>
2021-12-09 17:53:53 +0530

    react

---
+++ src/data.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line
+export default [
+  {
+    id: 1,
+    name: 'Ramesh',
+    age: 26,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/7d/Ramesh_Thilak.jpg'
+  },
+  {
+  id: 2,
+  name: 'suresh',
+  age: 42,
+  image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Gokul_Suresh.jpg/1200px-Gokul_Suresh.jpg'
+  },
+  {
+    id: 3,
+    name: 'dinesh',
+    age: 23,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Dinesh.Karthik.jpg'
+  },
+  {
+    id: 4,
+    name: 'naresh',
+    age: 44,
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Naresh-Vijaya-Krishna.jpg/1200px-Naresh-Vijaya-Krishna.jpg'
+  },
+  {
+    id: 5,
+    name: 'mukesh',
+    age: 31,
+    image: 'https://static.toiimg.com/thumb/imgsize-51373,msid-53338576,width-400,resizemode-4/53338576.jpg'
+  }
+    
+]
+    
